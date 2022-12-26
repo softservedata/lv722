71924710229ec1e0a9eaf9e84c6af01fac94dd8f
bebebe <hahaha@gmail.com>
2022-12-26 17:18:20 +0200

    Read Secret

--- src/test/java/edu/softserve/AppTest.java
+++ src/test/java/edu/softserve/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest {
         System.out.println("class AppTest; method testApp()");
         //
         System.out.println("***surefire.application.password = " + System.getProperty("surefire.application.password"));
-        System.out.println("***System.getenv().MY_PASSWORD = " + System.getenv().get("MY_PASSWORD"));
+        System.out.println("***System.getenv().MY_PASSWORD = " + System.getenv().get("secrets.MY_PASSWORD"));
         //
         Assert.assertTrue(true);
     }
