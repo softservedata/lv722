e8c982151acfba73922a2f925640ce051b7b7c83
Jaroslav <garasym@gmail.com>
2023-01-22 23:55:02 +0200

    add contact

--- src/main/java/com/softserve/Application.java
+++ src/main/java/com/softserve/Application.java
@@ -1,10 +1,12 @@
 package com.softserve;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@Slf4j
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     @Override
@@ -13,6 +15,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
+        log.info("***Start");
         SpringApplication.run(Application.class, args);
     }
 }
